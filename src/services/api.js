c31cf81fc5305c20ea1391b4d463834e10df50c8
Jean Crus <jean.crus@outlook.com>
2020-02-13 01:33:39 -0300

    Testing cors heroku

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://142.93.203.2/',
+    baseURL: 'https://cors-anywhere.herokuapp.com/http://142.93.203.2/',
 });
 
 export default api;
